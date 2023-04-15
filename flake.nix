7e25ac1116b66bf597d57c21a646af04a1301dee
Steeve Vu <steeve.vu@gmail.com>
2023-04-15 23:30:50 +0200

    flake: Remove `mach-nix` dependency

--- flake.nix
+++ flake.nix
@@ -4,35 +4,20 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    pypi-deps-db = {
-      url = "github:DavHau/pypi-deps-db";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.mach-nix.follows = "mach-nix";
-    };
-    mach-nix = {
-      url = "github:DavHau/mach-nix/3.5.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.pypi-deps-db.follows = "pypi-deps-db";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      mach = mach-nix.lib.${system};
-
-      myPython = mach.mkPython {
-        python = "python39";
-        requirements = builtins.readFile ./requirements.txt;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [
-          myPython
-        ];
-      };
-    }
-  );
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = pkgs.lib.attrsets.attrVals (
+            pkgs.lib.lists.init (
+              pkgs.lib.strings.splitString "\n" (builtins.readFile ./requirements.txt)
+            )
+          ) pkgs.python310Packages;
+        };
+      }
+    );
 }
