eb3ab0888bfffffb819c4edb799602fd842fe604
rx342 <15484682+rx342@users.noreply.github.com>
2024-01-06 11:50:48 +0100

    nix: Use `poetry2nix` from official repo

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, poetry2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+        poetryEnv = (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }).mkPoetryEnv {
           projectDir = ./.;
           preferWheels = true;
         };
